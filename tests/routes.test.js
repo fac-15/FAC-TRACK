84644dd8d8a9bc618e0cab513f6a01f0b714aa71
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-17 23:01:03 +0000

    damn router tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,19 +1,22 @@
 // jest works magically yay
-// const request = require("supertest");
-// const app = require("../src/app");
+const request = require("supertest");
+const app = require("../src/app");
+
+test("something passes at last", () => {
+    expect(1).toBe(1);
+});
+  
+
+// may want to modify how we are doing this, but it seems to work:
+// http://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/#7-About-the-Database-connection
 
 
-// // may want to modify how we are doing this, but it seems to work:
-// // http://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/#7-About-the-Database-connection
+test('home should return 200 status code with GET request', () => {
+    return request(app).get('/').then((res) => {
+        expect(res.statusCode).toBe(200);
+    })
+})
 
-// describe('Test the home route', () => {
-//     test('home should return 200 status code', () => {
-//         return request(app).get('/').then((res) => {
-//             expect(res.statusCode).toBe(200);
-//             // done();
-//         })
-//     })
-// })
 
 // describe('Test the dashboard route', () => {
 //     test('dashboard should return 200 status code', () => {
