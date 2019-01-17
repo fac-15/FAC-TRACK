c07df73c5e213e2f2b61e1df4d98b5dd1a17fde0
Hannah <hannahstewart23@yahoo.co.uk>
2019-01-17 12:09:40 +0000

    making a function to output the product of get all weeks to dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dbhelpers = require("../model/db_queries/index.js");
 
 // create a new router
 const router = express.Router();
@@ -8,50 +9,66 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
-
-
 // dashboard route
+// router.get("/dashboard", (req, res) => {
+//   dbhelpers.getAllWeeks((error, result) => {
+//     console.log("in getAllWeeks/dashboard route");
+//     if (error) {
+//       console.log("error in dbhelpers, router:", error);
+//     }
+//     console.log(result);
+//     res.render("dashboard", { weeks: result });
+//   });
+// });
+
 router.get("/dashboard", (req, res) => {
-  
-  // const name = req.params.name;
-  res.render("dashboard");
+  dbhelpers
+    .getAllWeeks()
+    .then(data => {
+      console.log("response from database", data);
+      res.render("dashboard", { weeks: data });
+    })
+    .catch(err => {
+      console.log("/dashboard error: ", err);
+    });
 });
 
+//   console.log(result);
+//   res.render("dashboard", { weeks: result });
+// });
 
 // week route(s)
-router.get('/week-:name(1_toolkit|2_testing|3_api|4_node-core|5_node-2|6_postgresql|7_authentication|8_express-handlebars)?', (req, res) => {
+router.get(
+  "/week-:name(1_toolkit|2_testing|3_api|4_node-core|5_node-2|6_postgresql|7_authentication|8_express-handlebars)?",
+  (req, res) => {
+    // write a database query to get task data (by week) for the user:
+    // - tasks complete
+    // - aggregate confidence
 
-  // write a database query to get task data (by week) for the user:
-  // - tasks complete
-  // - aggregate confidence
-  
-  // use the weekName to get tasks for the week
-  const weekName = req.params.name.split('_')[1];
-  res.render('week', {name: weekName});
-});
-
+    // use the weekName to get tasks for the week
+    const weekName = req.params.name.split("_")[1];
+    res.render("week", { name: weekName });
+  }
+);
 
 // task routes
 // 1 - 8
 
-
-
 // error pages
-router.use( (req, res) => {
+router.use((req, res) => {
   res.status(404);
 
-  if (req.accepts('html')) {
+  if (req.accepts("html")) {
     res.render("404");
     return;
   }
 
-  if (req.accepts('json')) {
+  if (req.accepts("json")) {
     res.render("500");
     return;
   }
 
-  res.type('txt').send('Not found');
+  res.type("txt").send("Not found");
 });
-
 
 module.exports = router;
