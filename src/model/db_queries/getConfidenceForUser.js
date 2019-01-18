574144cbaee8ee4c5efd7bc869d49af0695973f6
Hannah <hannahstewart23@yahoo.co.uk>
2019-01-18 15:22:28 +0000

    unfinished confidence agreagtor function

--- src/model/db_queries/getConfidenceForUser.js
+++ src/model/db_queries/getConfidenceForUser.js
@@ -14,4 +14,29 @@ const getConfidenceForUser = (user_id, weekId) =>
     );
   });
 
+const confidenceCal = confidence => {
+  console.log(typeof confidence);
+  const agregate = [];
+  console.log("log from confidenceCal helper :", confidence);
+  // console.log("logging week 2 confidence :", confidence[1]);
+
+  for (let i = 1; i < 3; i++) {
+    const result = confidence.filter(instance => instance.week_id === i);
+    // console.log(result);
+
+    let culmination = 0;
+    let count = 0;
+    for (let i = 0; i < result.length; i++) {
+      culmination += result[i].confidence;
+      count++;
+    }
+
+    const answer = culmination / count;
+    const object = { confidence: answer };
+    agregate.push(object);
+  }
+  console.log(agregate);
+  return agregate;
+};
+
 module.exports = getConfidenceForUser;
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,30 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+const confidenceCal = confidence => {
+  console.log(typeof confidence);
+  const agregate = [];
+  console.log("log from confidenceCal helper :", confidence);
+  // console.log("logging week 2 confidence :", confidence[1]);
+
+  for (let i = 1; i < 3; i++) {
+    const result = confidence.filter(instance => instance.week_id === i);
+    // console.log(result);
+
+    let culmination = 0;
+    let count = 0;
+    for (let i = 0; i < result.length; i++) {
+      culmination += result[i].confidence;
+      count++;
+    }
+
+    const answer = culmination / count;
+    const object = { confidence: answer };
+    agregate.push(object);
+  }
+  console.log(agregate);
+  return agregate;
+};
 // dashboard
 router.get("/dashboard/:id", (req, res) => {
   // enter user id to get:
