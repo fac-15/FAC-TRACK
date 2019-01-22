b802ff2785f3a389d43b09d1b257b9fab57d4e64
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-22 15:11:08 +0000

    dashboard sort of complete!

--- src/views/helpers/index.js
+++ src/views/helpers/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   trafficLight: require("./trafficLight"),
   taskComplete: require("./taskComplete"),
+  taskFraction: require("./taskFraction"),
    // weeksForUser: require("./taskComplete"),
   // tick_chevron: require("./tick_chevron")
 };
