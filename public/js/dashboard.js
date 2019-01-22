cdbc05fe20da933a8c22ff97832754feb3441cd1
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-22 14:57:21 +0000

    dashboard sort of complete!

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,24 +1,31 @@
 "use strict";
 
-const circle = document.querySelector('.progress-circle');
-const radius = circle.r.baseVal.value;
+
+// 1. get all circles
+const circles = Array.from(document.querySelectorAll('.progress-circle'));
+
+// 2. same for all circles: radius and circumference 
+const radius = circles[0].r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 
+// 3. same for all circles: dasharray and offset
+circles.map(circle => circle.style.strokeDasharray = `${circumference} ${circumference}`);
+circles.map(circle => circle.style.strokeDashoffset = `${circumference}`);
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = `${circumference}`;
 
-function setProgress(percent){
-    const offset = circumference - percent / 100 * circumference;
-    circle.style.strokeDashoffset = offset;
-}
+// 4. get all percentages from classes on svg elements
+// - this is an array of integers
+const icons = Array.from(document.querySelectorAll('.dashboard-icon'));
+const percentages = icons.map(icon => parseInt(icon.classList[1].split('_')[1]));
 
-const input = document.querySelector('input');
-
-input.addEventListener('change', function(e){
-
-    if(input.value< 101 && input.value > -1){
-        setProgress(input.value)
-    }
-
+// 5. animate each icon on load
+window.addEventListener("load", e => {
+    // loop over each percentage
+    percentages.map((pc, index) => {
+        // value can only be between 0 and 100
+        if (pc < 101 && pc > -1) {
+            const offset = circumference - pc / 100 * circumference;
+            circles[index].style.strokeDashoffset = offset;
+        }
+    })
 })
