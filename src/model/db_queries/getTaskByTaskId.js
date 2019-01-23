4b7e0f9b6e4fbeca4cce5c29c54053dd5c005d9d
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-01-23 12:41:29 +0000

    fix checkboxes, working on multiple ids from tasks

--- src/model/db_queries/getTaskByTaskId.js
+++ src/model/db_queries/getTaskByTaskId.js
@@ -1,10 +1,18 @@
 const db = require("../db/db_connection");
 
-const getTaskByTaskId = id => {
+// take a list of task ids
+const getTaskByTaskId = ids => {
+
+  const a = ids.toString();
+  console.log('string array: ', a)
+  console.log(ids.join(", "));
+
+  const joined = ids.join(", ");
+
   return new Promise((resolve, reject) => {
     db.query(
-      `SELECT t.id, name, task_slug, url_slug FROM tasks t INNER JOIN weeks w ON t.week_id = w.id WHERE t.id = $1`,
-      [id],
+      `SELECT t.id, name, task_slug, url_slug FROM tasks t INNER JOIN weeks w ON t.week_id = w.id WHERE t.id = ANY(?::INT[$1])`,
+      [ids],
       (err, res) => {
         if (err) {
           reject("error in getTaskByTaskId query", err);
